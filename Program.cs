b50719f2414b1c1074f77e8e50cc44e3183b7852
Alfred Larsson <alfred.co95@hotmail.com>
2022-10-10 12:50:43 +0200

    Accounts and Bankmenu added

--- Program.cs
+++ Program.cs
@@ -26,6 +26,33 @@ namespace ATM_Project
             Users[4, 0] = "Seb";
             Users[4, 1] = "0201";
 
+            decimal[][] Account = new decimal[5][];
+            Account[0] = new decimal[2];
+            Account[1] = new decimal[2];
+            Account[2] = new decimal[3];
+            Account[3] = new decimal[4];
+            Account[4] = new decimal[4];
+
+            Account[0][0] = 19042.42m;
+            Account[0][1] = 4000.21m;
+
+            Account[1][0] = 13235.40m;
+            Account[1][0] = 2000;
+
+            Account[2][0] = 5644.34m;
+            Account[2][1] = 2405.50m;
+            Account[2][2] = 64613.34m;
+
+            Account[3][0] = 12567.90m;
+            Account[3][1] = 735.34m;
+            Account[3][2] = 1395.20m;
+            Account[3][3] = 4041.12m;
+
+            Account[4][0] = 56456.21m;
+            Account[4][1] = 13550.78m;
+            Account[4][2] = 409.59m;
+            Account[4][3] = 1754.12m;
+
             int LogInAttempts = 3;
             bool CorrectCred = false;
             Console.WriteLine("Välkommen till banken, vänligen logga in");
@@ -56,9 +83,55 @@ namespace ATM_Project
             }
         }
 
-        public static void BankMenu()
+        public static void BankMenu(string UserName, string[,]Users)
         {
-
+            int Menu = 0;
+            bool LoggedIn = true;
+            do
+            {
+                try
+                {
+                    Console.WriteLine("Vänligen välj ett alternativ nedan");
+                    Console.WriteLine("1 - Se konton & saldo");
+                    Console.WriteLine("2 - Överföring av pengar");
+                    Console.WriteLine("3 - Ta ut pengar");
+                    Console.WriteLine("4 - Logga ut");
+                    Menu = int.Parse(Console.ReadLine());
+                    switch (Menu)
+                    {
+                        case 1:
+                            Console.Clear();
+                            //Accounts();
+                            Console.ReadKey();
+                            Console.Clear();
+                            break;
+                        case 2:
+                            //Transfer();
+                            Console.Clear();
+                            break;
+                        case 3:
+                            //Ta ut pengar
+                            Console.Clear();
+                            break;
+                        case 4:
+                            Console.WriteLine("Du har loggat ut.");
+                            System.Threading.Thread.Sleep(1500);
+                            LoggedIn = false;
+                            Console.Clear();
+                            Main(null);
+                            break;
+                        default:
+                            Console.WriteLine("Vänligen välj något av valen");
+                            Console.ReadKey();
+                            Console.Clear();
+                            break;
+                    }
+                }
+                catch
+                {
+                    Console.Clear();
+                }
+            } while (LoggedIn == true);
         }
     }
 }
