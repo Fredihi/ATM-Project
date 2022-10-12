4a9a5f65b0f49473848570fa3b4154e11f676660
Alfred Larsson <alfred.co95@hotmail.com>
2022-10-12 13:33:09 +0200

    Added comments and new methods, first method for the first choice in the menu.

--- Program.cs
+++ Program.cs
@@ -10,6 +10,7 @@ namespace ATM_Project
         }
         public static void LogIn()
         {
+            //A 2d string array for Users
             string[,] Users = new string[5, 2];
             Users[0, 0] = "Alfred";
             Users[0, 1] = "1234";
@@ -20,12 +21,13 @@ namespace ATM_Project
             Users[2, 0] = "Niklas";
             Users[2, 1] = "1337";
 
-            Users[3, 0] = "Mikaela";
+            Users[3, 0] = "Hilma";
             Users[3, 1] = "9999";
 
             Users[4, 0] = "Seb";
             Users[4, 1] = "0201";
 
+            //A jagged decimal array of Accounts
             decimal[][] Account = new decimal[5][];
             Account[0] = new decimal[2];
             Account[1] = new decimal[2];
@@ -37,7 +39,7 @@ namespace ATM_Project
             Account[0][1] = 4000.21m;
 
             Account[1][0] = 13235.40m;
-            Account[1][0] = 2000;
+            Account[1][1] = 2000;
 
             Account[2][0] = 5644.34m;
             Account[2][1] = 2405.50m;
@@ -54,39 +56,62 @@ namespace ATM_Project
             Account[4][3] = 1754.12m;
 
             int LogInAttempts = 3;
+            bool LoggedIn = false;
             bool CorrectCred = false;
-            Console.WriteLine("Välkommen till banken, vänligen logga in");
 
-            while (LogInAttempts != 0 && CorrectCred != true)
+            //Keeps program running until you've run out of attempts to log in.
+            while (LoggedIn == false && LogInAttempts != 0)
             {
-                Console.Write("Användarnamn:");
-                string UserName = Console.ReadLine();
-                Console.Write("Pinkod:");
-                string PinKod = Console.ReadLine();
-                //ExistingUser(Users, UserName, PinKod);
+                while (CorrectCred == false)//Runs this loop until you've entered the right credentials
+                {
+                    Console.WriteLine("Välkommen till banken, vänligen logga in.");
+                    Console.Write("Användarnamn:");
+                    string UserName = Console.ReadLine();
+                    Console.Write("Pinkod:");
+                    string PinKod = Console.ReadLine();
+                    ExistingUser(Users, UserName, PinKod);
+                    //If UserName exists and the right Pin has been entered it logs in to the program
+                    if (ExistingUser(Users, UserName, PinKod))
+                    {
+                        Console.Clear();
+                        LoggedIn = true;
+                        BankMenu(UserName, Users, Account, LoggedIn);
 
-                if (/*ExistingUser(Users, UserName, PinKod)*/)
-                {
-                    CorrectCred = true;
-                    BankMenu(UserName, Users);
-                }
-                else
-                {
-                    CorrectCred = false;
-                    LogInAttempts--;
-                }
-                while (LogInAttempts == 0)
-                {
-                    Console.WriteLine("Du har försökt för många gånger, försök igen senare");
-                    Console.ReadKey();
+
+                    }
+                    else if (!ExistingUser(Users, UserName, PinKod))
+                    {
+                        Console.WriteLine("Fel användarnamn eller pinkod, försök igen.");
+                        System.Threading.Thread.Sleep(500);//Pauses the console a little bit before letting you try again
+                        Console.Clear();
+                        CorrectCred = false;
+                        LogInAttempts--;
+                    }
+                    while (LogInAttempts == 0)
+                    {
+                        Console.WriteLine("Du har försökt för många gånger, försök igen senare");
+                        Console.ReadKey();
+                        Environment.Exit(0); //Exits the program if there are no more attempts
+                    }
                 }
             }
         }
+        //Checks if Username matches a user in Users array
+        static bool ExistingUser(string[,] Users, string UserName, string PinKod)
+        {
+            for (int i = 0; i < Users.GetLength(0); i++)
+            {
+                if (Users[i, 0] == UserName && Users[i, 1] == PinKod)
+                {
+                    return true; //If UserName and PinKod matches one in the Users array it will return true
+                }
+            }
+            return false;//If it does not match a user it will return false
+        }
 
-        public static void BankMenu(string UserName, string[,]Users)
+        public static void BankMenu(string UserName, string[,]Users, decimal[][] Account, bool LoggedIn)
         {
             int Menu = 0;
-            bool LoggedIn = true;
             do
             {
                 try
@@ -101,7 +126,7 @@ namespace ATM_Project
                     {
                         case 1:
                             Console.Clear();
-                            //Accounts();
+                            Accounts(UserName, Users, Account);
                             Console.ReadKey();
                             Console.Clear();
                             break;
@@ -115,10 +140,9 @@ namespace ATM_Project
                             break;
                         case 4:
                             Console.WriteLine("Du har loggat ut.");
-                            System.Threading.Thread.Sleep(1500);
-                            LoggedIn = false;
+                            System.Threading.Thread.Sleep(1500); //"Pauses" the console a little bit before "logging out"
+                            LoggedIn = false; //Changes LoggedIn bool to false so it can go back to "LogIn()" loop
                             Console.Clear();
-                            Main(null);
                             break;
                         default:
                             Console.WriteLine("Vänligen välj något av valen");
@@ -132,6 +156,32 @@ namespace ATM_Project
                     Console.Clear();
                 }
             } while (LoggedIn == true);
+        }
+        //Checks which user that is active and sets an index to it which we can use to edit the balance of the accounts of the user
+        public static int ActiveUser(string[,] Users, string UserName)
+        {
+            for (int i = 0; i < Users.GetLength(0); i++)
+            {
+                if (Users[i, 0] == UserName)
+                {
+                    return i;
+                }
+            }
+            return 0;
+        }
+        //Method to check balance of accounts
+        public static void Accounts(string UserName, string[,] Users, decimal[][] Account)
+        {
+            int IndexUser = ActiveUser(Users, UserName);//Index of the user that is logged in
+            string[] Acc = { "Lönekonto.", "Sparkonto.", "Semesterkonto.", "Pensionkonto." };//Array for all the accounts names
+            Console.WriteLine("Konton");
+            for (int i = 0; i < Account[IndexUser].Length; i++)//Loops throught the account of the user
+            {
+                if (Account[IndexUser][i] != 0)
+                {
+                    Console.WriteLine($"Du har {Account[IndexUser][i]}kr på ditt {Acc[i]} ");
+                }
+            }
         }
     }
 }
