49a463f25a0ca522278a48ebb2cdcd699f41e19e
Alfred Larsson <alfred.co95@hotmail.com>
2022-10-15 17:03:47 +0200

    Added method for bank transfers

--- Program.cs
+++ Program.cs
@@ -70,8 +70,8 @@ namespace ATM_Project
                     Console.Write("Pinkod:");
                     string PinKod = Console.ReadLine();
                     ExistingUser(Users, UserName, PinKod);
-                    //If UserName exists and the right Pin has been entered it logs in to the program
-                    if (ExistingUser(Users, UserName, PinKod))
+                    
+                    if (ExistingUser(Users, UserName, PinKod))//If UserName exists and the right Pin has been entered it logs in to the program
                     {
                         Console.Clear();
                         LoggedIn = true;
@@ -108,7 +108,7 @@ namespace ATM_Project
             }
             return false;//If it does not match a user it will return false
         }
-
+        //Method for the bankmenu
         public static void BankMenu(string UserName, string[,]Users, decimal[][] Account, bool LoggedIn)
         {
             int Menu = 0;
@@ -131,7 +131,7 @@ namespace ATM_Project
                             Console.Clear();
                             break;
                         case 2:
-                            //Transfer();
+                            Transfer(Users, Account, UserName);
                             Console.Clear();
                             break;
                         case 3:
@@ -172,7 +172,7 @@ namespace ATM_Project
         //Method to check balance of accounts
         public static void Accounts(string UserName, string[,] Users, decimal[][] Account)
         {
-            int IndexUser = ActiveUser(Users, UserName);//Index of the user that is logged in
+            int IndexUser = ActiveUser(Users, UserName);//Index of the user that is currently logged in
             string[] Acc = { "Lönekonto.", "Sparkonto.", "Semesterkonto.", "Pensionkonto." };//Array for all the accounts names
             Console.WriteLine("Konton");
             for (int i = 0; i < Account[IndexUser].Length; i++)//Loops throught the account of the user
@@ -182,6 +182,105 @@ namespace ATM_Project
                     Console.WriteLine($"Du har {Account[IndexUser][i]}kr på ditt {Acc[i]} ");
                 }
             }
+        }
+        //Method to transfer money from accounts within the user
+        public static void Transfer(string[,] Users, decimal[][] Account, string UserName)
+        {
+            Console.Clear();
+            int IndexUser = ActiveUser(Users, UserName);//Index of the user that is currently logged in
+            try
+            {
+                string[] Acc = { "Lönekonto", "Sparkonto", "Semesterkonto", "Pensionkonto" };//Array for all the accounts names
+                string[] AccChoice = { "1 - Lönekonto\n", "2 - Sparkonto\n", "3 - Semestersparande\n", "4 - Pension\n" };//Array for choices
+                Console.WriteLine("Ifrån vilket konto vill du flytta över ifrån?");
+                for (int i = 0; i < Account[IndexUser].Length; i++)
+                {
+                    foreach (var item in AccChoice[i])//Prints the choices that the use can choose from (depending on how many accounts)
+                    {
+                        Console.Write(item);
+                    }
+                }
+                int FirstCh = int.Parse(Console.ReadLine());//Which account to transfer from
+                switch (FirstCh)
+                {
+                    case 1:
+                        FirstCh = 0;
+                        break;
+                    case 2:
+                        FirstCh = 1;
+                        break;
+                    case 3:
+                        FirstCh = 2;
+                        break;
+                    case 4:
+                        FirstCh = 3;
+                        break;
+                }
+
+                Console.Clear();
+
+                Console.WriteLine("Vilket konto vill du flytta över till?");
+                for (int i = 0; i < Account[IndexUser].Length; i++)
+                {
+                    foreach (var item in AccChoice[i])//Prints the choices that the use can choose from (depending on how many accounts)
+                    {
+                        Console.Write(item);
+                    }
+                }
+                int SecondCh = int.Parse(Console.ReadLine());//Which account to transfer to
+                switch (SecondCh)
+                {
+                    case 1:
+                        SecondCh = 0;
+                        break;
+                    case 2:
+                        SecondCh = 1;
+                        break;
+                    case 3:
+                        SecondCh = 2;
+                        break;
+                    case 4:
+                        SecondCh = 3;
+                        break;
+                }
+
+                Console.Clear();
+                Console.WriteLine("Hur mycket vill du flytta över?");
+                decimal Sum = decimal.Parse(Console.ReadLine());//Amount to transfer
+                Console.Clear();
+
+                if (Sum > Account[IndexUser][FirstCh])//If the amount to transfer is more than what is available on the account
+                {
+                    Console.WriteLine("Beloppet du försöker överföra överskrider summan på kontot.");
+                }
+                else
+                {
+                    decimal A1 = Account[IndexUser][FirstCh] - Sum;//Amount to be removed from the first to choice stored in a variable
+                    decimal A2 = Account[IndexUser][SecondCh] + Sum;//Amount to be added to the second choice stored in a variable
+                    Account[IndexUser][FirstCh] = A1;//Declaring that the first account has been subtracted the amount stored in "Sum"
+                    Account[IndexUser][SecondCh] = A2;//Declaring that the second account has been added the amount stored in "Sum"
+                    Console.WriteLine($"Du har flyttat över {Sum} från ditt {Acc[FirstCh]} till ditt {Acc[SecondCh]}.");
+                    Console.WriteLine();
+                    Console.WriteLine("Nuvarande saldo på konton");
+
+                    for (int i = 0; i < Account[IndexUser].Length; i++)
+                    {
+                        if (Account[IndexUser][i] != 0)
+                        {
+                            Console.Write(Acc[i]);
+                            Console.Write(": ");
+                            Console.WriteLine(Account[IndexUser][i]);
+                        }
+                    }
+                }
+
+            }
+            catch
+            {
+                Console.Clear();
+                Console.WriteLine("Fel inmatning");
+            }
+            Console.ReadKey();
         }
     }
 }
